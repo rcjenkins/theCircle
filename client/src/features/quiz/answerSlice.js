429c5719626438f6a688ec905bee2a06812da8a6
Richard Jenkins <rcjenkins@hotmail.com>
2020-06-16 08:22:51 +0100

    some comments about Redux toolkit and Immer to avoid confusion

--- client/src/features/quiz/answerSlice.js
+++ client/src/features/quiz/answerSlice.js
@@ -9,6 +9,10 @@ export const answersSlice = createSlice({
   },
   reducers: {
     requestSaveAnswers: (state) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
       state.response = null;
       state.isFetching = true;
       state.error = null;
